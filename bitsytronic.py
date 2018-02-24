a111585357f2a50b166e4f2b82d8ac93d87de13c
mark <ragzouken@gmail.com>
2018-02-24 12:58:37 +0000

    full size

--- bitsytronic.py
+++ bitsytronic.py
@@ -8,11 +8,22 @@ DIGITS = "0123456789ABCDEF"
 def random_cell():
     return "".join(random.choice(DIGITS) for _ in xrange(4))
 
-def hex_digit_to_bits(digit):
-    return format(int(digit, base=16), '0>4b')
+def byte_to_bits(data):
+    return format(ord(data), '0>8b')
 
-def hex_grid_to_bools(grid):
-    return [bit == "1" for bit in "".join(hex_digit_to_bits(digit) for digit in grid)]
+def byte_grid_to_bools(grid):
+    return [bit == "1" for bit in "".join(byte_to_bits(byte) for byte in grid)]
+
+MIXER = [0, 16, 4, 20, 8, 24, 12, 28, 32, 48, 36, 52, 40, 56, 44, 60]
+
+def mix_grid(grid):
+    next = [False] * 64
+
+    for r, row in enumerate(MIXER): 
+        for i in xrange(4):
+            next[r * 4 + i] = grid[row + i]
+
+    return next
 
 def run():
     SCREEN = (480, 272)
@@ -27,25 +38,35 @@ def run():
     screen = pygame.display.set_mode(SCREEN)
     clock = pygame.time.Clock()
 
-    grid = [False] * 16
+    frame = 0
+    grids = [[False] * 64, [False] * 64]
+
     pipe = serial.Serial("COM3", timeout=0) 
     data = ""
+
+    while True:
+        if "\n" in pipe.read():
+            print("done")
+            break
+
+        clock.tick(FPS)
 
     while not EXIT:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 EXIT = True
 
-        data = pipe.readline().strip()
+        data += pipe.read(8 - len(data))
 
-        if len(data) == 4:
-            grid = hex_grid_to_bools(data)
+        if len(data) == 8:
+            grids[frame] = byte_grid_to_bools(data)
+            grids[frame] = mix_grid(grids[frame])
             data = ""
 
-        for y in xrange(4):
-            for x in xrange(4):
-                color = WHITE if grid[y * 4 + x] else BLACK
-                pygame.draw.rect(screen, color, (x * 64 + 8, y * 64 + 8, 64, 64))
+        for y in xrange(8):
+            for x in xrange(8):
+                color = WHITE if grids[frame][y * 8 + x] else BLACK
+                pygame.draw.rect(screen, color, (x * 32 + 8, y * 32 + 8, 32, 32))
 
         pygame.display.flip()
 
