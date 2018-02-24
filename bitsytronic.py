e82e9f5fc1dbb518320ebe924859fa8a94a4d3a0
mark <ragzouken@gmail.com>
2018-02-24 21:25:25 +0000

    it works

--- bitsytronic.py
+++ bitsytronic.py
@@ -2,11 +2,13 @@ import time
 import random
 import serial
 import pygame
+import itertools
 
-DIGITS = "0123456789ABCDEF"
-
-def random_cell():
-    return "".join(random.choice(DIGITS) for _ in xrange(4))
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return itertools.izip_longest(fillvalue=fillvalue, *args)
 
 def byte_to_bits(data):
     return format(ord(data), '0>8b')
@@ -14,16 +16,33 @@ def byte_to_bits(data):
 def byte_grid_to_bools(grid):
     return [bit == "1" for bit in "".join(byte_to_bits(byte) for byte in grid)]
 
-MIXER = [0, 16, 4, 20, 8, 24, 12, 28, 32, 48, 36, 52, 40, 56, 44, 60]
+MIXER   = [0, 16, 4, 20, 8, 24, 12, 28, 32, 48, 36, 52, 40, 56, 44, 60]
+UNMIXER = [0, 8, 16, 24, 4, 12, 20, 28, 32, 40, 48, 56, 36, 44, 52, 60] 
 
 def mix_grid(grid):
     next = [False] * 64
 
-    for r, row in enumerate(MIXER): 
+    for r, row in enumerate(MIXER):
         for i in xrange(4):
             next[r * 4 + i] = grid[row + i]
 
     return next
+
+def unmix_grid(grid):
+    next = [False] * 64
+
+    for r, row in enumerate(UNMIXER): 
+        for i in xrange(4):
+            next[r * 4 + i] = grid[row + i]
+
+    return next
+
+def send_grid(grid, pipe):
+    grid = unmix_grid(grid)
+
+    for chunk in grouper(grid, 8, 0):
+        value = int("".join(str(int(bit)) for bit in chunk)[::-1], 2)
+        pipe.write(chr(value))
 
 def run():
     SCREEN = (480, 272)
@@ -55,6 +74,10 @@ def run():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 EXIT = True
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    frame = 1 - frame
+                    send_grid(grids[frame], pipe)
 
         data += pipe.read(8 - len(data))
 
